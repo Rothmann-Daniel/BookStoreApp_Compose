2576a204b3ded6957cc02c28bb15cdf4399acc84
Daniel Rotman <danilarothmann@gmail.com>
2026-01-29 20:05:02 +0700

    Firebase_Auth: Layout_LoginScreen

---
+++ app/src/main/java/com/danielrothmann/bookstoreapp/auth/LoginScreen.kt
@@ -0,0 +1,25 @@
+package com.danielrothmann.bookstoreapp.auth
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen() {
+    val email = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+
+    }
+}
